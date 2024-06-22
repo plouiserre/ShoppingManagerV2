43b841806f110ad454083c38efc5e95158294ffe
Pierre-Louis Serré <pierre-louisserre@macbook-air-de-pierre-louis.home>
2024-06-22 13:34:35 +0200

    finish welcome page

--- shopping-manager-front/src/components/ListResume/ListResume.jsx
+++ shopping-manager-front/src/components/ListResume/ListResume.jsx
@@ -1,5 +1,18 @@
 import s from "./style.module.css";
+import { AddElement } from "../AddElement/AddElement";
 
 export function ListResume() {
-  return <h1>Listes de course enregistrées</h1>;
+  function simulateBehavior() {
+    alert("go to add new list");
+  }
+  return (
+    <div>
+      <h1>Listes de course enregistrées</h1>
+      <p className={`${s.text}`}>Aucun liste programmée pour le moment</p>
+      <AddElement
+        labelButton={"Ajouter une nouvelle liste"}
+        actionButton={simulateBehavior}
+      />
+    </div>
+  );
 }
--- shopping-manager-front/src/components/MealResume/MealResume.jsx
+++ shopping-manager-front/src/components/MealResume/MealResume.jsx
@@ -1,3 +1,18 @@
+import s from "./style.module.css";
+import { AddElement } from "../AddElement/AddElement";
+
 export function MealResume() {
-  return <h1>Repas programmés</h1>;
+  function simulateBehavior() {
+    alert("go to add new meal");
+  }
+  return (
+    <div>
+      <h1>Repas programmés</h1>
+      <p className={`${s.text}`}>Aucun liste programmée pour le moment</p>
+      <AddElement
+        labelButton={"Ajouter un nouveau repas"}
+        actionButton={simulateBehavior}
+      />
+    </div>
+  );
 }
