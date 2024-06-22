d6e24f510bdc37f676e33f0fc825f038ed13c8bc
Pierre-Louis Serré <pierre-louisserre@macbook-air-de-pierre-louis.home>
2024-06-22 20:27:03 +0200

    finish html array

--- shopping-manager-front/src/components/StockList/StockList.jsx
+++ shopping-manager-front/src/components/StockList/StockList.jsx
@@ -2,8 +2,16 @@ import s from "./style.module.css";
 import coffee from "../../assets/images/cafe.png";
 import meat from "../../assets/images/viande.png";
 import vegetables from "../../assets/images/legumes.png";
+import error from "../../assets/images/error.png";
+import ok from "../../assets/images/ok.png";
+import warning from "../../assets/images/warning.png";
+import sup from "../../assets/images/delete.png";
+import edit from "../../assets/images/edit.png";
 
 export function StockList() {
+  const nameFoodClasses = `${s.cellStockList} ${s.nameFood}`;
+  const indiceFoodClasses = `${s.cellStockList} ${s.indexFood}`;
+
   return (
     <div className={`${s.allStocks}`}>
       <div className={`row ${s.headerStockList}`}>
@@ -15,64 +23,94 @@ export function StockList() {
         <div className={`col-2 ${s.cellStockList}`}>Action</div>
       </div>
       <div className={`row ${s.stockListItem}`}>
-        <div className={`col-2 ${s.cellStockList}`}>1</div>
-        <div className={`col-2 ${s.cellStockList}`}>Carotte</div>
+        <div className={`col-2 ${indiceFoodClasses}`}>1</div>
+        <div className={`col-2 ${nameFoodClasses}`}>Carotte</div>
         <div className={`col-2 ${s.cellStockList}`}>
           <img src={vegetables} height="50px" width="50px" />
         </div>
-        <div className={`col-2 ${s.cellStockList}`}>2</div>
-        <div className={`col-2 ${s.cellStockList}`}>OK</div>
-        <div className={`col-2 ${s.cellStockList}`}>Supprimer Modifier</div>
+        <div className={`col-2 ${indiceFoodClasses}`}>2</div>
+        <div className={`col-2 ${s.cellStockList}`}>
+          <img src={ok} height="50px" width="50px" />
+        </div>
+        <div className={`col-2 ${s.cellStockList}`}>
+          <img src={sup} height="50px" width="50px" />{" "}
+          <img src={edit} height="50px" width="50px" />
+        </div>
       </div>
       <div className={`row ${s.stockListItem}`}>
-        <div className={`col-2 ${s.cellStockList}`}>2</div>
-        <div className={`col-2 ${s.cellStockList}`}>Lapin</div>
+        <div className={`col-2 ${indiceFoodClasses}`}>2</div>
+        <div className={`col-2 ${nameFoodClasses}`}>Lapin</div>
         <div className={`col-2 ${s.cellStockList}`}>
           <img src={meat} height="50px" width="50px" />
         </div>
-        <div className={`col-2 ${s.cellStockList}`}>3</div>
-        <div className={`col-2 ${s.cellStockList}`}>OK</div>
-        <div className={`col-2 ${s.cellStockList}`}>Supprimer Modifier</div>
+        <div className={`col-2 ${indiceFoodClasses}`}>3</div>
+        <div className={`col-2 ${s.cellStockList}`}>
+          <img src={ok} height="50px" width="50px" />
+        </div>
+        <div className={`col-2 ${s.cellStockList}`}>
+          <img src={sup} height="50px" width="50px" />
+          <img src={edit} height="50px" width="50px" />
+        </div>
       </div>
       <div className={`row ${s.stockListItem}`}>
-        <div className={`col-2 ${s.cellStockList}`}>3</div>
-        <div className={`col-2 ${s.cellStockList}`}>Boeuf</div>
+        <div className={`col-2 ${indiceFoodClasses}`}>3</div>
+        <div className={`col-2 ${nameFoodClasses}`}>Boeuf</div>
         <div className={`col-2 ${s.cellStockList}`}>
           <img src={meat} height="50px" width="50px" />
         </div>
-        <div className={`col-2 ${s.cellStockList}`}>2</div>
-        <div className={`col-2 ${s.cellStockList}`}>Périmé</div>
-        <div className={`col-2 ${s.cellStockList}`}>Supprimer Modifier</div>
+        <div className={`col-2 ${indiceFoodClasses}`}>2</div>
+        <div className={`col-2 ${s.cellStockList}`}>
+          <img src={error} height="50px" width="50px" />
+        </div>
+        <div className={`col-2 ${s.cellStockList}`}>
+          <img src={sup} height="50px" width="50px" />
+          <img src={edit} height="50px" width="50px" />
+        </div>
       </div>
       <div className={`row ${s.stockListItem}`}>
-        <div className={`col-2 ${s.cellStockList}`}>4</div>
-        <div className={`col-2 ${s.cellStockList}`}>Yaourt</div>
+        <div className={`col-2 ${indiceFoodClasses}`}>4</div>
+        <div className={`col-2 ${nameFoodClasses}`}>Yaourt</div>
         <div className={`col-2 ${s.cellStockList}`}>
           <img src={coffee} height="50px" width="50px" />
         </div>
-        <div className={`col-2 ${s.cellStockList}`}>8</div>
-        <div className={`col-2 ${s.cellStockList}`}>OK</div>
-        <div className={`col-2 ${s.cellStockList}`}>Supprimer Modifier</div>
+        <div className={`col-2 ${indiceFoodClasses}`}>8</div>
+        <div className={`col-2 ${s.cellStockList}`}>
+          <img src={ok} height="50px" width="50px" />
+        </div>
+        <div className={`col-2 ${s.cellStockList}`}>
+          <img src={sup} height="50px" width="50px" />
+          <img src={edit} height="50px" width="50px" />
+        </div>
       </div>
       <div className={`row ${s.stockListItem}`}>
-        <div className={`col-2 ${s.cellStockList}`}>5</div>
-        <div className={`col-2 ${s.cellStockList}`}>Taboulé</div>
+        <div className={`col-2 ${indiceFoodClasses}`}>5</div>
+        <div className={`col-2 ${nameFoodClasses}`}>Taboulé</div>
         <div className={`col-2 ${s.cellStockList}`}>
           <img src={vegetables} height="50px" width="50px" />
         </div>
-        <div className={`col-2 ${s.cellStockList}`}>1</div>
-        <div className={`col-2 ${s.cellStockList}`}>OK</div>
-        <div className={`col-2 ${s.cellStockList}`}>Supprimer Modifier</div>
+        <div className={`col-2 ${indiceFoodClasses}`}>1</div>
+        <div className={`col-2 ${s.cellStockList}`}>
+          <img src={ok} height="50px" width="50px" />
+        </div>
+        <div className={`col-2 ${s.cellStockList}`}>
+          <img src={sup} height="50px" width="50px" />
+          <img src={edit} height="50px" width="50px" />
+        </div>
       </div>
       <div className={`row ${s.stockListItem}`}>
-        <div className={`col-2 ${s.cellStockList}`}>6</div>
-        <div className={`col-2 ${s.cellStockList}`}>Brocolis</div>
+        <div className={`col-2 ${indiceFoodClasses}`}>6</div>
+        <div className={`col-2 ${nameFoodClasses}`}>Brocolis</div>
         <div className={`col-2 ${s.cellStockList}`}>
           <img src={vegetables} height="50px" width="50px" />
         </div>
-        <div className={`col-2 ${s.cellStockList}`}>5</div>
-        <div className={`col-2 ${s.cellStockList}`}>OK</div>
-        <div className={`col-2 ${s.cellStockList}`}>Supprimer Modifier</div>
+        <div className={`col-2 ${indiceFoodClasses}`}>5</div>
+        <div className={`col-2 ${s.cellStockList}`}>
+          <img src={warning} height="50px" width="50px" />
+        </div>
+        <div className={`col-2 ${s.cellStockList}`}>
+          <img src={sup} height="50px" width="50px" />
+          <img src={edit} height="50px" width="50px" />
+        </div>
       </div>
     </div>
   );
