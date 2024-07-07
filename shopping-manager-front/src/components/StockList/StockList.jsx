e270853e1f7c79980fb0b49d28fb641ad04e7c6d
Pierre-Louis Serré <pierre-louisserre@macbook-air-de-pierre-louis.home>
2024-07-07 17:03:20 +0200

    link table stock with add form

--- shopping-manager-front/src/components/StockList/StockList.jsx
+++ shopping-manager-front/src/components/StockList/StockList.jsx
@@ -1,52 +1,9 @@
 import s from "./style.module.css";
 import { StockListItem } from "../StockListItem/StockListItem";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 export function StockList() {
-  //const stocksBis = useSelector((store) => store.STOCK.stocks);
-  const stocks = [];
-  stocks.push({
-    Id: 1,
-    Name: "Carotte",
-    Type: "vegetables",
-    Quantity: 2,
-    Status: "ok",
-  });
-  stocks.push({
-    Id: 2,
-    Name: "Lapin",
-    Type: "meat",
-    Quantity: 3,
-    Status: "ok",
-  });
-  stocks.push({
-    Id: 3,
-    Name: "Boeuf",
-    Type: "meat",
-    Quantity: 2,
-    Status: "error",
-  });
-  stocks.push({
-    Id: 4,
-    Name: "Yaourt",
-    Type: "breakfast",
-    Quantity: 8,
-    Status: "ok",
-  });
-  stocks.push({
-    Id: 5,
-    Name: "Taboule",
-    Type: "vegetables",
-    Quantity: 1,
-    Status: "ok",
-  });
-  stocks.push({
-    Id: 6,
-    Name: "Brocolis",
-    Type: "vegetables",
-    Quantity: 5,
-    Status: "warning",
-  });
+  const stocks = useSelector((store) => store.STOCK.stocks);
 
   function clickElementName(name) {
     alert("You go to the page of " + name);
