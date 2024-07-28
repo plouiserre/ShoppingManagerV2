0b748f805d31b31a2a4aa03520f4c97a6dc32aca
Pierre-Louis Serré <pierre-louisserre@Hostacc906029e5f.proxynet.ladapt.local>
2024-07-28 17:06:32 +0200

    css detail page and structure html

--- shopping-manager-front/src/pages/Stock/StockDetail.jsx
+++ shopping-manager-front/src/pages/Stock/StockDetail.jsx
@@ -3,41 +3,30 @@ import { Pictogramme } from "../../components/Pictogramme/Pictogramme";
 
 export function StockDetail() {
   return (
-    <div className={`container-fluid ${s.formStock}`}>
-      <div className={`row ${s.lineDetail}`}>
+    <div className={`container-fluid ${s.detailStock}`}>
+      <div class="row">
         <div className="col-3"></div>
-        <div className="col-7">
-          <h1 className={`${s.titleForm}`}>Stock Element</h1>
+        <div className="col-2">
+          <Pictogramme pictoName={"vegetables"} height={100} width={100} />
+        </div>
+        <div className={`col-4 ${s.stockName}`}>
+          <div>Patate</div>
         </div>
         <div className="col-2"></div>
       </div>
-      <div className={`row ${s.lineDetail}`}>
+      <div className={`row ${s.quantityStock}`}>
         <div className="col-3"></div>
-        <div className="col-3">Nom</div>
-        <div className={`col-4 ${s.nameStock}`}>Patate</div>
-        <div className="col-2"></div>
+        <div className="col-2">Quantité</div>
+        <div className={`col-3 ${s.quantityNumber}`}>2</div>
+        <div className="col-3"></div>
       </div>
-      <div className={`row ${s.lineDetail}`}>
+      <div className={`row ${s.quantityStock}`}>
         <div className="col-3"></div>
-        <div className="col-3">Type</div>
-        <div className="col-4">
-          <Pictogramme pictoName={"vegetables"} height={50} width={50} />
-        </div>
-        <div className="col-2"></div>
-      </div>
-      <div className={`row ${s.lineDetail}`}>
-        <div className="col-3"></div>
-        <div className="col-3">Quantité</div>
-        <div className="col-4">2</div>
-        <div className="col-2"></div>
-      </div>
-      <div className={`row ${s.lineDetail}`}>
-        <div className="col-3"></div>
-        <div className="col-3">Status</div>
-        <div className="col-4">
+        <div className="col-2">Status</div>
+        <div className={`col-3 ${s.quantityNumber}`}>
           <Pictogramme pictoName={"ok"} height={50} width={50} />
         </div>
-        <div className="col-2"></div>
+        <div className="col-3"></div>
       </div>
     </div>
   );
