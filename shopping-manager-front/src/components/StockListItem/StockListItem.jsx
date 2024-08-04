e9ea2fd450f6ad97f8f5b81f350342767d54c600
Pierre-Louis Serré <pierre-louisserre@Hostacc906029e5f.proxynet.ladapt.local>
2024-08-04 14:52:03 +0200

    redesign delete button

--- shopping-manager-front/src/components/StockListItem/StockListItem.jsx
+++ shopping-manager-front/src/components/StockListItem/StockListItem.jsx
@@ -30,22 +30,24 @@ export function StockListItem({ element, clickName }) {
       <div className={`col-2 ${s.cellStockList}`}>
         <Status element={element} />
       </div>
-      {/* <div className={`col-2 ${s.cellStockList}`}>
-        <input
-          className={`form-check-input ${s.deleteStockItem}`}
-          type="checkbox"
-          value=""
-          checked={selected}
-          id="flexCheckDefault"
-          onChange={(e) => selectStockElement(element, e)}
-        ></input>
-      </div> */}
       <div className={`col-2 ${s.cellStockList}`}>
-        <CustomButton
-          labelButton={"Supprimer élément"}
-          actionButton={() => deleteStock(element)}
-          customClass="btn btn-danger"
-        />
+        <button
+          type="button"
+          class="btn btn-outline-danger"
+          onClick={() => deleteStock(element)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="42"
+            height="42"
+            fill="currentColor"
+            class="bi bi-trash"
+            viewBox="0 0 16 16"
+          >
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"></path>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"></path>
+          </svg>
+        </button>
       </div>
     </div>
   );
