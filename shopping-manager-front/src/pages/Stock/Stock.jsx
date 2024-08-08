ad342441401a592aed824dd3753f33562515793c
Pierre-Louis Serré <pierre-louisserre@Hostacc906029e5f.proxynet.ladapt.local>
2024-08-08 17:24:28 +0200

    delete expired stock

--- shopping-manager-front/src/pages/Stock/Stock.jsx
+++ shopping-manager-front/src/pages/Stock/Stock.jsx
@@ -1,16 +1,34 @@
 import { CustomButton } from "../../components/CustomButton/CustomButton";
 import { StockList } from "../../containers/StockList/StockList";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { deleteExpiredStock } from "../../store/stock/stock-slice";
 
 export function Stock() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  function deleteActionExpiredStock() {
+    dispatch(deleteExpiredStock());
+  }
   return (
     <>
       <StockList />
-      <CustomButton
-        labelButton={"Ajouter un nouvel élément"}
-        actionButton={() => navigate("/stock/add/")}
-      />
+      <div class="row">
+        <div class="col-4">
+          <CustomButton
+            labelButton={"Ajouter un nouvel élément"}
+            actionButton={() => navigate("/stock/add/")}
+          />
+        </div>
+        <div class="col-6"></div>
+        <div class="col-2">
+          <CustomButton
+            labelButton={"Supprimer stock périmé"}
+            actionButton={() => deleteActionExpiredStock()}
+            customClass={"btn btn-danger"}
+          />
+        </div>
+      </div>
     </>
   );
 }
